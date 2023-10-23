243d7721c38ecc407fc8d35c6fee36455e0ad435
Devdutt Mehrol <113674752+DevD1k@users.noreply.github.com>
2023-10-23 15:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
 // your code here
-const btn = document.getElementById('button');
-		const text = document.querySelector('.text');
-		const num = document.querySelector('.number');
-		const head = document.getElementById('url');
-		btn.addEventListener('click', function() {
-			if(text.value && num.value) {
-				 head.textContent = `https://localhost:8080/?name=${text.value}&year=${num.value}`;
-			}else if(text.value) {
-				head.textContent = `https://localhost:8080/?name=${text.value}`;
-			}else if(num.value) {
-				head.textContent = `https://localhost:8080/?year=${num.value}`;
-			}else {
-				head.textContent = `nigga`;
-			}
-		})
